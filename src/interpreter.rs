30c06bda8a5d2e39b2a605fb5a469dd5cf4eb2e0
Shigma <shigma10826@gmail.com>
2024-06-24 18:00:57 +0800

    feat: eval for operators

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -7,17 +7,17 @@ pub trait Interpreter {
     fn parse_pattern(&self, input: &str) -> Result<(Box<dyn Pattern>, usize), SyntaxError>;
 }
 
-pub trait Expr: Debug {}
+pub trait Expr: Debug + Any {}
 
-pub trait Pattern: Debug {}
+pub trait Pattern: Debug + Any {}
 
-pub trait Context {
+pub trait Context: Any {
     fn eval(&self, expr: &dyn Expr) -> Result<Box<dyn Value>, /* Self::Error */ Box<dyn Any>>;
     fn fork(&self) -> Box<dyn Context>;
     fn bind(&mut self, pattern: &dyn Pattern, value: Box<dyn Value>) -> Result<(), /* Self::Error */ Box<dyn Any>>;
 }
 
-pub trait Value {
+pub trait Value: Any {
     fn to_string(&self) -> Result<String, /* Self::Error */ Box<dyn Any>>;
     fn to_bool(&self) -> Result<bool, /* Self::Error */ Box<dyn Any>>;
     fn to_entries(&self) -> Result<Vec<(Box<dyn Value>, Box<dyn Value>)>, /* Self::Error */ Box<dyn Any>>;
@@ -27,9 +27,10 @@ pub mod default {
     use std::any::Any;
 
     use pest::Parser;
-    use serde_json::{json, Value as JsonValue};
+    use serde_json::{json, Number, Value as JsonValue};
 
     use crate::error::SyntaxError;
+    use super::Value as _;
 
     #[derive(Parser)]
     #[grammar = "default.pest"]
@@ -59,49 +60,153 @@ pub mod default {
         }
     }
 
+    #[derive(Debug, Clone, Copy)]
+    pub enum UnaryOp {
+        Not,
+        Pos,
+        Neg,
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    pub enum BinaryOp {
+        Pow,
+        Mul, Div, Mod,
+        Add, Sub,
+        Shl, Shr, UShr,
+        Lt, Le, Gt, Ge,
+        Eq, Ne,
+        BitAnd,
+        BitXor,
+        BitOr,
+        And,
+        Or,
+    }
+
     #[derive(Debug)]
-    pub struct Expr {}
+    pub enum Expr {
+        Number(Number),
+        String(String),
+        Ident(String),
+        Array(Vec<Expr>),
+        Apply(Box<Expr>, Vec<Expr>),
+        Unary(UnaryOp, Box<Expr>),
+        Binary(Box<Expr>, BinaryOp, Box<Expr>),
+    }
 
     impl super::Expr for Expr {}
 
     #[derive(Debug)]
-    pub struct Pattern {}
+    pub enum Pattern {
+        Ident(String),
+    }
 
     impl super::Pattern for Pattern {}
 
     pub struct Context {
-        value: JsonValue,
+        inner: JsonValue,
+    }
+
+    impl Context {
+        fn _eval(&self, expr: &Expr) -> Result<Value, Box<dyn Any>> {
+            let inner = match expr {
+                Expr::Number(n) => JsonValue::Number(n.clone()),
+                Expr::String(s) => JsonValue::String(s.clone()),
+                Expr::Ident(ident) => {
+                    if ident == "true" {
+                        JsonValue::Bool(true)
+                    } else if ident == "false" {
+                        JsonValue::Bool(false)
+                    } else if ident == "null" {
+                        JsonValue::Null
+                    } else {
+                        self.inner.get(ident).unwrap_or(&JsonValue::Null).clone()
+                    }
+                },
+                // Expr::Array(vec) => {
+                //     let vec = vec.iter().map(|expr| {
+                //         self._eval(expr).unwrap()
+                //     }).collect();
+                //     JsonValue::Array(vec)
+                // },
+                // Expr::Apply(func, args) => {
+                //     let func = self._eval(func).unwrap();
+                //     let args = args.iter().map(|expr| {
+                //         self._eval(expr).unwrap()
+                //     }).collect();
+                //     JsonValue::Null
+                // },
+                Expr::Unary(op, expr) => {
+                    let value = self._eval(expr).unwrap();
+                    match op {
+                        UnaryOp::Not => JsonValue::Bool(!value.to_bool()?),
+                        UnaryOp::Pos => JsonValue::Number(value.to_number()?),
+                        // UnaryOp::Neg => JsonValue::Number(match value.to_number()? {
+                        //     Number::PosInt(n) => Number::NegInt(-n),
+                        //     Number::NegInt(n) => Number::PosInt(-n),
+                        //     Number::Float(n) => Number::Float(-n),
+                        // }),
+                        _ => unimplemented!(),
+                    }
+                },
+                Expr::Binary(lhs, op, rhs) => {
+                    let lhs = self._eval(lhs).unwrap();
+                    let rhs = self._eval(rhs).unwrap();
+                    match op {
+                        _ => unimplemented!(),
+                    }
+                },
+                _ => unimplemented!(),
+            };
+            Ok(Value{ inner })
+        }
     }
 
     impl super::Context for Context {
-        fn eval(&self, _expr: &dyn super::Expr) -> Result<Box<dyn super::Value>, Box<dyn Any>> {
-            Ok(Box::new(Value {
-                value: JsonValue::Null,
-            }))
+        fn eval(&self, expr: &dyn super::Expr) -> Result<Box<dyn super::Value>, Box<dyn Any>> {
+            let expr = (expr as &dyn Any).downcast_ref::<Expr>().unwrap();
+            Ok(Box::new(self._eval(expr)?))
         }
 
         fn fork(&self) -> Box<dyn super::Context> {
             Box::new(Context {
-                value: self.value.clone(),
+                inner: self.inner.clone(),
             })
         }
 
-        fn bind(&mut self, _pattern: &dyn super::Pattern, _value: Box<dyn super::Value>) -> Result<(), /* Self::Error */ Box<dyn Any>> {
-            todo!()
+        fn bind(&mut self, pattern: &dyn super::Pattern, value: Box<dyn super::Value>) -> Result<(), /* Self::Error */ Box<dyn Any>> {
+            match (pattern as &dyn Any).downcast_ref::<Pattern>().unwrap() {
+                Pattern::Ident(ident) => {
+                    self.inner[ident] = (value as Box<dyn Any>).downcast::<Value>().unwrap().inner;
+                    Ok(())
+                },
+            }
         }
     }
 
     pub struct Value {
-        value: JsonValue,
+        inner: JsonValue,
+    }
+
+    impl Value {
+        fn to_number(&self) -> Result<Number, Box<dyn Any>> {
+            match &self.inner {
+                JsonValue::Number(n) => Ok(n.clone()),
+                _ => Err(Box::new(())),
+            }
+        }
     }
 
     impl super::Value for Value {
         fn to_string(&self) -> Result<String, Box<dyn Any>> {
-            Ok(self.value.to_string())
+            match &self.inner {
+                JsonValue::Null => Ok("".to_string()),
+                // TODO partial object
+                _ => Ok(self.inner.to_string()),
+            }
         }
 
         fn to_bool(&self) -> Result<bool, Box<dyn Any>> {
-            match &self.value {
+            match &self.inner {
                 JsonValue::Null => Ok(false),
                 JsonValue::Bool(b) => Ok(*b),
                 JsonValue::Number(n) => Ok(n.as_i64().unwrap() != 0),
@@ -112,15 +217,15 @@ pub mod default {
         }
 
         fn to_entries(&self) -> Result<Vec<(Box<dyn super::Value>, Box<dyn super::Value>)>, /* Self::Error */ Box<dyn Any>> {
-            match &self.value {
+            match &self.inner {
                 JsonValue::Array(vec) => Ok(vec.iter().enumerate().map(|(k, v)| {
-                    let value = Box::new(Value { value: v.clone() }) as Box<dyn super::Value>;
-                    let key = Box::new(Value { value: json!(k) }) as Box<dyn super::Value>;
+                    let value = Box::new(Value { inner: v.clone() }) as Box<dyn super::Value>;
+                    let key = Box::new(Value { inner: json!(k) }) as Box<dyn super::Value>;
                     (value, key)
                 }).collect()),
                 JsonValue::Object(map) => Ok(map.iter().map(|(k, v)| {
-                    let value = Box::new(Value { value: v.clone() }) as Box<dyn super::Value>;
-                    let key = Box::new(Value { value: json!(k) }) as Box<dyn super::Value>;
+                    let value = Box::new(Value { inner: v.clone() }) as Box<dyn super::Value>;
+                    let key = Box::new(Value { inner: json!(k) }) as Box<dyn super::Value>;
                     (value, key)
                 }).collect()),
                 _ => Err(Box::new(())),
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(trait_upcasting)]
+
 #[macro_use]
 extern crate pest_derive;
 
